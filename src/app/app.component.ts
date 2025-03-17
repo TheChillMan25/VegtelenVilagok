e78fb9dfddeca3f0f9afdcd3e874dfb1e37a2318
Gyurcsó-Licsman Péter <h357395@stud.u-szeged.hu>
2025-03-17 09:02:45 +0100

    Navbar shared mappába helyezés

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 import { HomeComponent } from './pages/home/home.component';
 import { FajokComponent } from './pages/fajok/fajok.component';
 
