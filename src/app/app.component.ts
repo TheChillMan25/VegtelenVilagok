3074d2443556070763f33d2ed3b17f784383b0b6
Gyurcsó-Licsman Péter <h357395@stud.u-szeged.hu>
2025-03-27 15:41:45 +0100

    tisztogatás

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,14 @@
 import { Component, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/navbar/navbar.component';
-import { HomeComponent } from './pages/home/home.component';
 import { FajokComponent } from './pages/fajok/fajok.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, HomeComponent, FajokComponent],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  @ViewChild(FajokComponent) fajokComponent!: FajokComponent;
   title = 'Végtelen Világok';
-
-  page = 'home';
-  prevPage = 'home';
-
-  changePage(page: string) {
-    this.prevPage = this.page;
-    if (page === 'fajok' && this.fajokComponent) {
-      this.fajokComponent.changeFaj('');
-    }
-    this.page = page;
-  }
-
-  onBack() {
-    if (this.page === 'fajok' && this.fajokComponent.currentFaj !== '') {
-      this.fajokComponent.changeFaj('');
-    } else{
-      this.page = this.prevPage;
-    }
-  }
 }
